149fb10514bb27efb385287b5dfda0cca0767fe3
fahadmunawar <fahadshaikh006@gmail.com>
2023-10-18 11:13:17 -0400

    Added beginning of story

--- CCStoryTemplate/Chapter 1/Chapter1.swift
+++ CCStoryTemplate/Chapter 1/Chapter1.swift
@@ -4,7 +4,9 @@
 //
 
 import Foundation
+import SwiftUI
 
 func chapterOne() {
     // Your portion of the story goes here
+    print("Once upon a time there was a young prince that was the next heir to his fathers throne.")
 }
