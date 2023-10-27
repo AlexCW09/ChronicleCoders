c0260b6ab3b3ae45a9a084876c88a568b75d3f18
Alex Wash <Washingtonalex6901@gmail.com>
2023-10-27 10:39:21 -0400

    few changes and added while loop

---
+++ CCStoryTemplate/Chapter 6/Chapter 6.swift
@@ -0,0 +1,8 @@
+//
+//  Chapter 6.swift
+//  ChronicleCoders
+//
+//  Created by Alexander Washington on 10/27/23.
+//
+
+import Foundation
---
+++ CCStoryTemplate/Chapter 7/Chapter 7.swift
@@ -0,0 +1,8 @@
+//
+//  Chapter 7.swift
+//  ChronicleCoders
+//
+//  Created by Alexander Washington on 10/27/23.
+//
+
+import Foundation
